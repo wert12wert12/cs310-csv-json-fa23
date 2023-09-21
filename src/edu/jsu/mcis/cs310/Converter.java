123c3cd7fd97a4b3609942bc786787080bcb7531
jobshua <jobshua@192.168.1.211>
2023-09-20 23:25:51 -0500

    Made csvtojson and jsontocsv, not passing tests, but format not matching somehow.

--- src/edu/jsu/mcis/cs310/Converter.java
+++ src/edu/jsu/mcis/cs310/Converter.java
@@ -1,5 +1,9 @@
 package edu.jsu.mcis.cs310;
 
+import java.io.StringWriter;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
 import com.github.cliftonlabs.json_simple.*;
 import com.opencsv.*;
 
@@ -74,36 +78,123 @@ public class Converter {
     @SuppressWarnings("unchecked")
     public static String csvToJson(String csvString) {
         
-        String result = "{}"; // default return value; replace later!
+        //String result = "{}"; // default return value; replace later!
+        
+        JsonObject Obj = new JsonObject();
+        
+        JsonArray ProdNums = new JsonArray();
+        
+        JsonArray ColHeadings = new JsonArray();
+        
+        JsonArray Data = new JsonArray();
         
         try {
-        
-            // INSERT YOUR CODE HERE
+            
+            String[] Lines = csvString.split("\n");
+            
+            // Convert lines to json arrays.
+            
+            // remove first array as column headers
+            
+            //
+            
+            for (int i = 0; i < Lines.length; i++) {
+                // "6149-02","Where No Man Has Gone Before","1","01","1312.4 - 1313.8","9/22/1966","1/20/2007"
+                // ->
+                // ["6149-02","Where No Man Has Gone Before","1","01","1312.4 - 1313.8","9/22/1966","1/20/2007"]
+                var LineData = Lines[i].replace("\"", "").split(",");
+                
+                if (i == 0) {
+                    for (int d = 0; d < LineData.length; d++) {
+                        ColHeadings.add(LineData[d]);
+                    }
+                    
+                    continue;
+                }
+                
+                ProdNums.add(LineData[0]);
+                
+                JsonArray DataArr = new JsonArray();
+                
+                for (int d = 1; d < LineData.length; d++) {
+                    DataArr.add(LineData[d]);
+                }
+                
+                Data.add(DataArr);
+            }
+            
+            Obj.put("ProdNums", ProdNums);
+            Obj.put("ColHeadings", ColHeadings);
+            Obj.put("Data", Data); 
+            
+            
+            
             
         }
         catch (Exception e) {
             e.printStackTrace();
         }
         
-        return result.trim();
+        return Obj.toJson();
         
     }
     
     @SuppressWarnings("unchecked")
     public static String jsonToCsv(String jsonString) {
+       
+        StringWriter writer = new StringWriter();
         
-        String result = ""; // default return value; replace later!
+        CSVWriter csvWriter = new CSVWriter(writer);
         
         try {
             
-            // INSERT YOUR CODE HERE
+            List<String[]> data = new ArrayList<String[]>();
             
+            JsonObject parsedData = Jsoner.deserialize(jsonString, new JsonObject());
+            
+            JsonArray ColHeadings = (JsonArray) parsedData.get("ColHeadings");
+            JsonArray ProdNums = (JsonArray) parsedData.get("ProdNums");
+            JsonArray Datas = (JsonArray) parsedData.get("Data");
+            
+            // add column headers.
+            String columnName = ColHeadings.toString();
+            String[] columnNameArray = columnName.substring(1, columnName.length() - 1).split(", ");
+            data.add(columnNameArray);
+            //adding production numbers
+            String productionNum = ProdNums.toString();
+            String[] proNumArray = productionNum.substring(1, productionNum.length() - 1).split(", ");
+            
+            
+            //adding the rest of the data
+            String datas = Datas.toString();
+            String[] datasArray = datas.substring(1, datas.length() - 1).split("],");
+           
+            List<String[]> allTogether = new ArrayList();
+            
+            // loop over prodnums
+            // grab data from prodnums and episode data.
+            for (int i = 0; i < proNumArray.length; i++) {
+                List<String> whatever = new ArrayList();
+                
+                String ProdNum = proNumArray[i];
+                String[] DataData = datasArray[i].substring(2, datasArray[i].length() - 1).split(", ");
+                
+                whatever.add(ProdNum);
+                whatever.addAll(Arrays.asList(DataData));
+                String[] arr = whatever.toArray(new String[0]);
+                allTogether.add(arr);
+            }
+            
+            data.addAll(allTogether);
+            
+            csvWriter.writeAll(data);
+            csvWriter.close();
         }
         catch (Exception e) {
             e.printStackTrace();
         }
         
-        return result.trim();
+        return writer.toString();
         
     }
     
